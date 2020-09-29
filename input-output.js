b999bf595c45a061345c09b38292cd743195ec82
Thiago2020 <thiagorodrigo276@yahoo.com>
2020-09-28 21:50:54 -0300

    Primeiro commite do codigo

---
+++ input-output.js
@@ -0,0 +1,5 @@
+var nome, idade;
+nome = prompt("Digite seu nome:");
+idade = prompt("Digite sua Idade:");
+alert("O nome digitado foi:" +nome);
+alert("A idade digitada foi:" +idade);
---
+++ operadores relacionais.js
@@ -0,0 +1,27 @@
+/*
+Var
+// Seção de Declarações das variáveis 
+      nota01, nota02: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+      nota01 := 10
+      nota02 := 15
+      
+      se(nota01 <> nota02)entao
+      escreval("O BAgulho é loko")
+      senao
+      escreval("O Bagulho é zika pivete!")
+  fimse */
+
+  var nota01, nota02;
+
+
+  nota01 = 10
+  nota02 = 15
+  
+  if(nota01 == nota02)
+  alert("O BAgulho é loko")
+  else
+  alert("O Bagulho é zika pivete!")
+
